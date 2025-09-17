2cc5db61def1483c7466446f55ee3db93d17f7f7
NyoRyorN <jy13.27.813@gmail.com>
2025-09-17 20:15:31 +0900

    背景設定

--- diet-frontend/layouts/default.vue
+++ diet-frontend/layouts/default.vue
@@ -1,7 +1,11 @@
+<script lang="js" setup>
+const backgroundImageUrl = ref("/images/cool_backgrounds.png")
+</script>
+
 <template>
     <div class="results-page">
         <div class="results-header">
-            <h1>Results Page</h1>
+            <h1 class="results-title">Results Page</h1>
             <NuxtLink to="/" class="tohome">Go back to Home</NuxtLink>
         </div>
         <div class="results-content">
@@ -10,15 +14,20 @@
     </div>
 </template>
 
-<style scoped>
+<style lang="css">
 .results-page {
     font-family: 'Helvetica Neue', Arial, sans-serif;
     margin: 0;
     display: flex;
     flex-direction: column;
+    width: calc(100vw - 16px);
+    height: calc(100vh - 16px);
+}
+
+.results-title {
+    margin: 0;
+    padding: 0;
     width: 100%;
-    height: 100%;
-    background-image: url("/images/cool_background.jpg");
 }
 
 .results-header {
@@ -29,9 +38,14 @@
 }
 
 .results-content {
-    background-color: #f9f9f9;
+    /* background-color: #f9f9f9; */
     padding: 20px;
     border-radius: 5px;
+    height: 100%;
+    background-image: url(/images/cool_backgrounds.png);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 }
 .tohome {
     text-decoration: none;
